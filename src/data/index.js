5c28351612d98f0d18311bb746f4450f863e0924
vdnghia03 <voduyanqn1972@gmail.com>
2025-12-24 09:33:52 +0700

    cd: modified to update data

--- src/data/index.js
+++ src/data/index.js
@@ -3,7 +3,12 @@ import { lesson2 } from './lesson2';
 import { lesson3} from './lesson3';
 import { lesson4} from './lesson4';
 import { lesson5} from './lesson5';
-// Sau này có lesson3 thì import tiếp ở đây
+import { lesson6} from './lesson6';
+import { lesson7} from './lesson7';
+import { lesson8} from './lesson8';
+import { lesson9} from './lesson9';
+import { lesson10} from './lesson10';
+
 
 // Xuất ra mảng chứa tất cả bài học
 export const lessonsData = [
@@ -11,5 +16,11 @@ export const lessonsData = [
   lesson2,
   lesson3,
   lesson4,
-  lesson5
+  lesson5,
+  lesson6,
+  lesson7,
+  lesson8,
+  lesson9,
+  lesson10,
+
 ];
